019d5f90cc2ce490a0be3cc76cfc8ed63118dee7
Jonathan Ismael Zapata Sandoval <84453014+JonathanZapata1@users.noreply.github.com>
2023-01-14 19:56:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dagbook/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.example.dagbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+    final static int SPLASH_SCREEN=4500;
+    Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        logo=findViewById(R.id.logo);
+        //animacion principal
+        rotateAnimation();
+        logo.animate().translationX(-3000).setDuration(1000).setStartDelay(3500);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(getApplicationContext(),LogoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+
+    private void rotateAnimation() {
+        animation= AnimationUtils.loadAnimation(this,R.anim.rotate_anim);
+        logo.startAnimation (animation);
+
+    }
+}
