a3e2d8b4eab2d97dc0d5e22bda88c6eb6574cbfc
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2023-01-24 11:17:19 -0500

    calendar

--- app/src/main/java/com/example/dagbook/activity/ui/calendar/GalleryFragment.java
+++ app/src/main/java/com/example/dagbook/activity/ui/calendar/GalleryFragment.java
@@ -1,32 +1,38 @@
 package com.example.dagbook.activity.ui.calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.dagbook.R;
+import com.example.dagbook.activity.ProspectActivity;
 import com.example.dagbook.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
-
+    View view;
     private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        view=inflater.inflate(R.layout.fragment_home, container, false);
+        Button calendar = (Button) view.findViewById(R.id.btnAgregar);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProspectActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
     @Override
@@ -34,4 +40,6 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
